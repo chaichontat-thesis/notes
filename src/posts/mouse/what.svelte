eab05689956b0bb999d9c97a81b52ac4a248a7c1
Chaichontat Sriworarat <34997334+chaichontat@users.noreply.github.com>
2022-06-17 16:35:50 -0400

    PNG step
    Signed-off-by: Chaichontat Sriworarat <34997334+chaichontat@users.noreply.github.com>

---
+++ src/posts/mouse/what.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import PngViewer from "$src/lib/components/pngViewer.svelte";
+  import c from "./xtrials0pos.json";
+  import c1 from "./xtrials1pos.json";
+
+  //   const c = JSON.parse(header);
+  let pos = 100;
+</script>
+
+<div class="flex gap-x-6">
+  <span>
+    Neuron: <input type="number" bind:value={pos} class="ml-1 w-24 rounded border bg-neutral-50 py-1 px-2" />
+  </span>
+  <input class="flex-grow" type="range" bind:value={pos} min="0" max={c.idx.length - 1} step="1" />
+</div>
+<div class="h-5" />
+<aside>Sequential</aside>
+<PngViewer url="https://chaichontat-host.s3.amazonaws.com/movie/xtrials0.bin" header={c} {pos} />
+<aside class="mt-3">Scrambled</aside>
+<div class="mt-4">
+  <PngViewer url="https://chaichontat-host.s3.amazonaws.com/movie/xtrials1.bin" header={c1} {pos} xlabel="Step" />
+</div>
